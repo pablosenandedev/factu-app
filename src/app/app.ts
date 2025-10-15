f436d75388fbc0fb8d5d2aebb873616a8ac062bf
Pablo <pablosenandedev@gmail.com>
2025-10-15 16:23:27 +0200

    clean app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,21 +9,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('factu-app');
-
-  private http = inject(HttpClient);
-
-  private apiUrl = 'http://127.0.0.1:8000/api/v1';
-  private meUrl = `${this.apiUrl}/auth/me`;
-
-  constructor() {
-    this.http.get<{ message: string }>(this.meUrl ).subscribe({
-      next: (response) => {
-        this.title.set(response.message);
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      }
-    });
-  }
-
 }
