ad34713dd9134e863b71fae5d9317eccdc243634
Pablo <pablosenandedev@gmail.com>
2025-10-15 11:59:24 +0200

    Add httpclient and test it

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([])  // You can add interceptors here
+    )
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('factu-app');
+
+  private http = inject(HttpClient);
+
+  private apiUrl = 'http://127.0.0.1:8000/api/v1';
+  private meUrl = `${this.apiUrl}/auth/me`;
+
+  constructor() {
+    this.http.get<{ message: string }>(this.meUrl ).subscribe({
+      next: (response) => {
+        this.title.set(response.message);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
+
 }
